b75195974f38691cdedb967d319aa71d69a4611f
Femi <pasival14@gmail.com>
2025-07-30 12:20:52 +0100

    deploy to vercel

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,12 +14,14 @@ export const authOptions = {
     strategy: "jwt" as const,
   },
   callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async session({ session, token }: { session: any; token: any }) {
       if (token && session.user) {
         session.user.id = token.sub!;
       }
       return session;
     },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async jwt({ token, user }: { token: any; user: any }) {
       if (user) {
         token.sub = user.id;
