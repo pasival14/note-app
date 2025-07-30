2cc99167f02fcaaad901eb9fe704eddc2fc13171
Femi <pasival14@gmail.com>
2025-07-30 10:59:15 +0100

    deploy to vercel

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -12,7 +12,7 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
